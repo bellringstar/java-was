303c626acb29cf02229f161a9152ebda0f50f943
bellringstar <bellringstar@gmail.com>
2024-07-02 13:26:24 +0900

    feat: FileReader.java 베이스 파일 설정 path가 폴더로 될 시 해당 폴더에서 index.html을 베이스 파일로 설정해 찾도록 변경 없는 파일일 경우 FileNotFoundException 발생

--- src/main/java/codesquad/webserver/FileReader.java
+++ src/main/java/codesquad/webserver/FileReader.java
@@ -1,6 +1,7 @@
 package codesquad.webserver;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,28 +9,29 @@ import org.slf4j.LoggerFactory;
 public class FileReader {
     private static Logger logger = LoggerFactory.getLogger(FileReader.class);
     private static final String STATIC_DIRECTORY = "static";
+    private static final String BASE_FILE = "index.html";
 
-    public File read(String requestPath) {
+    public File read(String requestPath) throws FileNotFoundException {
         String fileName = requestPath.equals("/") ? "index.html" : requestPath.substring(1);
-
         URL resource = getClass().getClassLoader().getResource(STATIC_DIRECTORY + "/" + fileName);
 
         if (resource == null) {
             logger.error("File not found: " + fileName);
-            throw new RuntimeException("File not found: " + fileName);
+            throw new FileNotFoundException("File not found: " + fileName);
         }
-
-        logger.info("Reading file: " + resource.getPath());
 
         File file = new File(resource.getFile());
 
-        if (!file.exists() || !file.isFile()) {
-            logger.error("File not found: " + resource.getFile());
-            throw new RuntimeException("File not found: " + resource.getFile());
+        if (!file.isFile()) {
+            file = new File(resource.getPath() + "/" + BASE_FILE);
+        }
+
+        if (!file.exists()) {
+            logger.error("File not found: " + fileName);
+            throw new FileNotFoundException("File not found: " + fileName);
         }
 
         logger.info("File found: " + file.getPath());
         return file;
-
     }
 }
