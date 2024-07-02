f5b58d98030e5e7ea928131cab2769f708d13c89
bellringstar <bellringstar@gmail.com>
2024-07-02 16:56:46 +0900

    feat: 저장소를 가저와 저장하는 기능 추가 요청이 create경로로 오면 파라미터에 값을 사용해 User를 생성하고 저장소에 저장

--- src/main/java/codesquad/webserver/RequestHandler.java
+++ src/main/java/codesquad/webserver/RequestHandler.java
@@ -1,5 +1,8 @@
 package codesquad.webserver;
 
+import codesquad.webserver.db.UserDatabase;
+import codesquad.webserver.db.UserDatabaseFactory;
+import codesquad.webserver.model.User;
 import codesquad.webserver.parser.BodyParser;
 import codesquad.webserver.parser.HeaderParser;
 import codesquad.webserver.parser.HttpParser;
@@ -37,6 +40,14 @@ public class RequestHandler {
                 HttpRequest request = httpParser.parse(in);
                 logger.debug("Received request: " + request.requestLine().method() + " " + request.requestLine().path());
 
+                // create로 시작하는 경로 -> 파라미터에 있는 정보로 유저 객체 생성 후 메인 페이지로 send
+                if (request.requestLine().path().startsWith("/create")) {
+                    saveUser(request);
+                    HttpResponse response = responseBuilder.buildRedirectResponse("/");
+                    sendResponse(out, response);
+                    return;
+                }
+
                 File file = fileReader.read(request.requestLine().path());
                 HttpResponse response = responseBuilder.build(file);
 
@@ -54,5 +65,11 @@ public class RequestHandler {
     private void sendResponse(OutputStream out, HttpResponse response) throws IOException {
         out.write(response.getByte());
         out.flush();
+    }
+
+    private void saveUser(HttpRequest request) {
+        User user = User.of(request.params());
+        UserDatabase instance = UserDatabaseFactory.getInstance();
+        instance.save(user);
     }
 }
