feb871570d75e27789bae815eec64293b8bd1e04
bellringstar <bellringstar@gmail.com>
2024-07-02 16:55:05 +0900

    feat: HttpRequest.java 객체에 필드 추가 params를 위한 필드 추가

--- src/main/java/codesquad/webserver/HttpRequest.java
+++ src/main/java/codesquad/webserver/HttpRequest.java
@@ -3,6 +3,6 @@ package codesquad.webserver;
 import codesquad.webserver.parser.RequestLine;
 import java.util.Map;
 
-public record HttpRequest(RequestLine requestLine, Map<String, String> headers, String body) {
+public record HttpRequest(RequestLine requestLine, Map<String, String> headers, Map<String, String> params, String body) {
 
 }
