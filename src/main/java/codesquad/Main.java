ceffc2e433aa21459b91faecd8fc2532750df685
bellringstar <bellringstar@gmail.com>
2024-07-04 13:34:29 +0900

    refact: RouteConfig 추가 라우팅 하는 부분을 메인 메서드에서 별도로 분리

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -2,23 +2,9 @@ package codesquad;
 
 import codesquad.webserver.FileReader;
 import codesquad.webserver.WebServer;
+import codesquad.webserver.config.RouteConfig;
 import codesquad.webserver.dispatcher.HttpRequestDispatcher;
 import codesquad.webserver.httpresponse.HttpResponseWriter;
-import codesquad.webserver.parser.BodyParser;
-import codesquad.webserver.parser.HeaderParser;
-import codesquad.webserver.parser.HttpParser;
-import codesquad.webserver.parser.QueryStringParser;
-import codesquad.webserver.parser.RequestLineParser;
-import codesquad.webserver.requesthandler.HomeRequestHandler;
-import codesquad.webserver.requesthandler.RegisterRequestHandler;
-import codesquad.webserver.requesthandler.StaticFileHandler;
-import codesquad.webserver.requesthandler.UserCreateRequestHandler;
-import codesquad.webserver.router.Router;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.URL;
 
 
 public class Main {
@@ -35,23 +21,7 @@ public class Main {
     }
 
     private static void init() {
-        Router router = Router.getInstance();
-        FileReader reader = new FileReader();
-        StaticFileHandler staticFileHandler = new StaticFileHandler(reader);
-
-        router.addRoute("*.css", staticFileHandler);
-
-        router.addRoute("*.js", staticFileHandler);
-
-        router.addRoute("*.png", staticFileHandler);
-        router.addRoute("*.jpg", staticFileHandler);
-        router.addRoute("*.gif", staticFileHandler);
-
-        router.addRoute("/img/*", staticFileHandler);
-        router.addRoute("/favicon.ico", staticFileHandler);
-
-        router.addRoute("/index.html", new HomeRequestHandler(reader));
-        router.addRoute("/register.html", new RegisterRequestHandler(reader));
-        router.addRoute("/create", new UserCreateRequestHandler(reader));
+        RouteConfig routeConfig = new RouteConfig(new FileReader());
+        routeConfig.configureRoutes();
     }
 }
