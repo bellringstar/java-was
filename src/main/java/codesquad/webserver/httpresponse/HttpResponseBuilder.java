098c8ea41aa7ada5561c644f9745c2812cfe6ef0
bellringstar <bellringstar@gmail.com>
2024-07-04 12:26:48 +0900

    feat: 예외 리스폰스 추가 허용되지 않는 메서드 혹은 서버 에러 발생 시 리턴하는 리스폰스 추가

--- src/main/java/codesquad/webserver/httpresponse/HttpResponseBuilder.java
+++ src/main/java/codesquad/webserver/httpresponse/HttpResponseBuilder.java
@@ -1,4 +1,4 @@
-package codesquad.webserver;
+package codesquad.webserver.httpresponse;
 
 import java.io.File;
 import java.io.IOException;
@@ -6,7 +6,7 @@ import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
 
-public class HttpResponseBuilder {
+public abstract class HttpResponseBuilder {
     private static final Map<String, String> MIME_TYPES = new HashMap<>();
     static {
         MIME_TYPES.put("html", "text/html");
@@ -19,7 +19,7 @@ public class HttpResponseBuilder {
         MIME_TYPES.put("ico", "image/x-icon");
     }
 
-    public HttpResponse build(File file) throws IOException {
+    public static HttpResponse build(File file) throws IOException {
         if (file.exists() && file.isFile()) {
             byte[] body = Files.readAllBytes(file.toPath());
             String contentType = getContentType(file.getName());
@@ -30,10 +30,10 @@ public class HttpResponseBuilder {
 
             return new HttpResponse(200, "OK", headers, body);
         }
-        return buildNotFondResponse();
+        return buildNotFoundResponse();
     }
 
-    public HttpResponse buildNotFondResponse() {
+    public static HttpResponse buildNotFoundResponse() {
         String body = "<html><body><h1>Not Fond</h1></body></html>";
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "text/html");
@@ -42,13 +42,21 @@ public class HttpResponseBuilder {
         return new HttpResponse(404, "Not Found", headers, body.getBytes());
     }
 
-    public HttpResponse buildRedirectResponse(String location) {
+    public static HttpResponse buildMethodErrorResponse() {
+        return new HttpResponse(405, "Method Not Allowed");
+    }
+
+    public static HttpResponse buildServerErrorResponse() {
+        return new HttpResponse(500, "Internal Server Error");
+    }
+
+    public static HttpResponse buildRedirectResponse(String location) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Location", location);
         return new HttpResponse(302, "Found", headers, new byte[0]);
     }
 
-    private String getContentType(String fileName) {
+    private static String getContentType(String fileName) {
         int dotIndex = fileName.lastIndexOf('.');
         if (dotIndex > 0 && dotIndex < fileName.length() - 1) {
             String extension = fileName.substring(dotIndex + 1).toLowerCase();
