7f10f5a4f266193d27dd5841edf6a08da5c08354
bellringstar <bellringstar@gmail.com>
2024-07-11 21:35:15 +0900

    feat: db 싱그톤으로 관리 모든 유저를 검색하는 메서드 추가

--- src/main/java/codesquad/webserver/db/user/InMemoryUserDatabase.java
+++ src/main/java/codesquad/webserver/db/user/InMemoryUserDatabase.java
@@ -2,8 +2,10 @@ package codesquad.webserver.db.user;
 
 import codesquad.webserver.annotation.Component;
 import codesquad.webserver.model.User;
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,6 +14,10 @@ public class InMemoryUserDatabase implements UserDatabase {
 
     private static final Logger logger = LoggerFactory.getLogger(InMemoryUserDatabase.class);
     private final ConcurrentHashMap<String, User> users = new ConcurrentHashMap<>();
+
+    public InMemoryUserDatabase() {
+        users.put("1", new User("1", "1", "1"));
+    }
 
     @Override
     public void save(User user) {
@@ -25,6 +31,11 @@ public class InMemoryUserDatabase implements UserDatabase {
     public User findByUserId(String userId) {
         return Optional.ofNullable(users.get(userId))
                 .orElseThrow(() -> new IllegalArgumentException("User with id " + userId + " not found"));
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        return users.values().stream().collect(Collectors.toUnmodifiableList());
     }
 
     @Override
