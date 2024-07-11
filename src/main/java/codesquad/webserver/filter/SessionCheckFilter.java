d1bc2dd7b7887c53254b0739ecde9ff302e146ab
bellringstar <bellringstar@gmail.com>
2024-07-11 13:41:16 +0900

    feat: 세션 검증 로직 추가 쿠키에 들어있는 정보를 기반으로 세션매니저에 있는지 판단하는 로직 추가

--- src/main/java/codesquad/webserver/filter/SessionCheckFilter.java
+++ src/main/java/codesquad/webserver/filter/SessionCheckFilter.java
@@ -3,6 +3,11 @@ package codesquad.webserver.filter;
 import codesquad.webserver.httprequest.HttpRequest;
 import codesquad.webserver.httpresponse.HttpResponse;
 import codesquad.webserver.httpresponse.HttpResponseBuilder;
+import codesquad.webserver.session.InMemorySessionManager;
+import codesquad.webserver.session.Session;
+import codesquad.webserver.session.SessionManager;
+import java.util.List;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,7 +31,25 @@ public class SessionCheckFilter implements Filter {
     }
 
     private boolean hasValidSession(HttpRequest request) {
-        return request.headers().containsKey(SESSION_KEY);
+        List<String> cookie = request.headers().get("Cookie");
+        if (cookie == null || cookie.isEmpty()) {
+            return false;
+        }
+
+        Optional<String> sessionId = cookie.stream()
+                .flatMap(c -> List.of(c.split(";")).stream())
+                .map(String::trim)
+                .filter(c -> c.startsWith(SESSION_KEY + "="))
+                .map(c -> c.substring((SESSION_KEY + "=").length()))
+                .findFirst();
+
+        if (sessionId.isEmpty()) {
+            return false;
+        }
+
+        SessionManager sessionManager = InMemorySessionManager.getInstance();
+        Session session = sessionManager.getSession(sessionId.get());
+        return session != null;
     }
 
     @Override
