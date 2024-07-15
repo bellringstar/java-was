1321b333606833c20c34170be119cf862744f654
bellringstar <bellringstar@gmail.com>
2024-07-15 21:54:36 +0900

    feat: WriteRequestHandler
    로컬에 이미지 파일 저장하는 기능 추가

--- src/main/java/codesquad/webserver/dispatcher/requesthandler/WriteRequestHandler.java
+++ src/main/java/codesquad/webserver/dispatcher/requesthandler/WriteRequestHandler.java
@@ -3,13 +3,22 @@ package codesquad.webserver.dispatcher.requesthandler;
 import codesquad.webserver.annotation.Autowired;
 import codesquad.webserver.annotation.Controller;
 import codesquad.webserver.annotation.RequestMapping;
+import codesquad.webserver.db.article.Article;
+import codesquad.webserver.db.article.ArticleDatabase;
+import codesquad.webserver.db.article.Image;
 import codesquad.webserver.dispatcher.view.ModelAndView;
 import codesquad.webserver.dispatcher.view.ModelKey;
 import codesquad.webserver.dispatcher.view.ViewName;
 import codesquad.webserver.filereader.FileReader;
 import codesquad.webserver.filereader.FileReader.FileResource;
 import codesquad.webserver.httprequest.HttpRequest;
+import codesquad.webserver.httprequest.HttpRequest.FileItem;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,10 +28,22 @@ public class WriteRequestHandler extends AbstractRequestHandler {
 
     private static final String PATH = "/article/write.html";
     private static final Logger logger = LoggerFactory.getLogger(WriteRequestHandler.class);
+    private static final String UPLOAD_DIR_NAME = "uploads";
+    private final String uploadDir;
+
+    private final ArticleDatabase articleDatabase;
 
     @Autowired
-    public WriteRequestHandler(FileReader fileReader) {
+    public WriteRequestHandler(FileReader fileReader, ArticleDatabase articleDatabase) {
         super(fileReader);
+        this.articleDatabase = articleDatabase;
+        this.uploadDir = getProjectUploadPath();
+        logger.info("Upload directory set to: {}", this.uploadDir);
+    }
+
+    private String getProjectUploadPath() {
+        String projectDir = System.getProperty("user.dir");
+        return Paths.get(projectDir, UPLOAD_DIR_NAME).toString();
     }
 
     @Override
@@ -50,6 +71,60 @@ public class WriteRequestHandler extends AbstractRequestHandler {
 
     @Override
     protected ModelAndView handlePost(HttpRequest request) {
-        return super.handlePost(request);
+        Map<String, String> multipartFields = request.getMultipartFields();
+        Map<String, FileItem> multipartFiles = request.getMultipartFiles();
+
+        String content = multipartFields.get("content");
+        FileItem imageFile = multipartFiles.get("image");
+
+        try {
+            Article article = new Article(content);
+
+            if (imageFile != null) {
+                String imagePath = saveImageToLocal(imageFile);
+                Image image = new Image(null, imagePath, null);
+                article.addImage(image);
+            }
+
+            articleDatabase.save(article);
+
+            ModelAndView mv = new ModelAndView(ViewName.REDIRECT_VIEW);
+            mv.addAttribute(ModelKey.REDIRECT_URL, "/");
+            return mv;
+
+        } catch (IOException e) {
+            logger.error("Error saving image: {}", e.getMessage());
+            return new ModelAndView(ViewName.EXCEPTION_VIEW)
+                    .addAttribute(ModelKey.STATUS_CODE, 500)
+                    .addAttribute(ModelKey.ERROR_MESSAGE, "Error saving image");
+        } catch (Exception e) {
+            logger.error("Error saving article: {}", e.getMessage());
+            return new ModelAndView(ViewName.EXCEPTION_VIEW)
+                    .addAttribute(ModelKey.STATUS_CODE, 500)
+                    .addAttribute(ModelKey.ERROR_MESSAGE, "Error saving article");
+        }
+    }
+
+    private String saveImageToLocal(FileItem imageFile) throws IOException {
+        String fileName = UUID.randomUUID() + "_" + imageFile.getFilename();
+        File uploadDirFile = new File(uploadDir);
+
+        if (!uploadDirFile.exists()) {
+            if (!uploadDirFile.mkdirs()) {
+                logger.error("Failed to create upload directory: {}", uploadDir);
+                throw new IOException("Failed to create upload directory: " + uploadDir);
+            }
+        }
+
+        File file = new File(uploadDirFile, fileName);
+
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            fos.write(imageFile.getContent());
+        } catch (IOException e) {
+            logger.error("Error writing file: {}", e.getMessage(), e);
+            throw e;
+        }
+
+        return file.getAbsolutePath();
     }
 }
