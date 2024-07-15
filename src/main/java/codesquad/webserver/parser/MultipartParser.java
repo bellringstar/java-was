82dacde50ae1b554eacd4593957ca94f266ad6a5
bellringstar <bellringstar@gmail.com>
2024-07-15 20:08:39 +0900

    fix: MultipartParser
    사용하지 않는 상수 제거

--- src/main/java/codesquad/webserver/parser/MultipartParser.java
+++ src/main/java/codesquad/webserver/parser/MultipartParser.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Map;
 
 public class MultipartParser {
-    private static final int BUFFER_SIZE = 8192;
 
     public static void parse(BufferedReader reader, HttpRequest request) throws IOException {
         String boundary = extractBoundary(request.getHeaders().get("Content-Type"));
