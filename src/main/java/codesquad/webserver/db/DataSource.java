cc2a67072c98656da4f50af50074b133460329a3
bellringstar <bellringstar@gmail.com>
2024-07-15 20:24:44 +0900

    feat: DataSource
    데이터베이스 연결 추상화

---
+++ src/main/java/codesquad/webserver/db/DataSource.java
@@ -0,0 +1,8 @@
+package codesquad.webserver.db;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface DataSource {
+    Connection getConnection() throws SQLException;
+}
