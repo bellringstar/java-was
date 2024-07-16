389100b47a718e5c1292f496099f22c1193661ae
bellringstar <bellringstar@gmail.com>
2024-07-16 14:30:50 +0900

    feat: Article
    작성자 칼럼 추가

--- src/main/java/codesquad/webserver/db/article/Article.java
+++ src/main/java/codesquad/webserver/db/article/Article.java
@@ -1,5 +1,6 @@
 package codesquad.webserver.db.article;
 
+import codesquad.webserver.db.user.User;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,10 +8,23 @@ public class Article {
     private Long id;
     private String title;
     private String content;
+    private User author;
     private List<Image> images = new ArrayList<>();
 
     public Article(String title, String content) {
         this(null, title, content);
+    }
+
+    public Article(String title, String content, User author) {
+        this(null, title, content, author, new ArrayList<>());
+    }
+
+    public Article(Long id, String title, String content, User author, List<Image> images) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.images = images;
     }
 
     public Article(Long id, String title, String content) {
@@ -19,11 +33,8 @@ public class Article {
         this.content = content;
     }
 
-    public Article(Long id, String title, String content, List<Image> images) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.images = images;
+    public User getAuthor() {
+        return author;
     }
 
     public Long getId() {
@@ -48,5 +59,16 @@ public class Article {
 
     public void addImage(Image image) {
         images.add(image);
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", author=" + author +
+                ", images=" + images +
+                '}';
     }
 }
